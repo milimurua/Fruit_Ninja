f4eb6e48d071f107854d9ad8a073987c81994206
milimurua <milagrosmurua2004@gmail.com>
2025-06-19 11:14:56 -0300

    fixed errors

---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    public GameObject whole;
+    public GameObject sliced;
+
+    private Rigidbody fruitRigibody;
+    private Collider fruitCollider;
+
+    private void Slice(Vector3 direction, Vector3 position, float force)
+    {
+        whole.SetActive(false);
+        sliced.SetActive(true);
+
+        fruitCollider.enabled = false;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        sliced.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+        Rigidbody[] slice = sliced.GetComponentsInChildren<Rigidbody>();
+
+        foreach (Rigidbody Slice in slices)
+        {
+            slice.velocity = fruitRigibody.velocity;
+            slice.addForceAtPosition()
+        }
+
+    }
+    
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Blade blade = other.GetComponent<Blade>();
+            Slice(blade.direction, blade.transform.position, blade.sliceForce);
+        }
+    }
+}
